3d62e1d5e176371619cd80ebaa6eaac02ad5beb1
Adam <sinegamy11@gmail.com>
2020-07-05 21:37:43 +0700

    fix

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const wrap = document.querySelector('.wrap');
+const DomElement = function(selector, height, width, bg, fontSize) {
+	 this.selector = selector;
+	 this.height = height;
+	 this.width = width;
+	 this.bg = bg;
+	 this.fontSize = fontSize;
+}
+const element = new DomElement('.class-name', '100px', '370px', 'red', '16px');
+DomElement.prototype.createTag = function() {
+	const addSelector = function(elem) {
+		elem.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
+		elem.style.cssText = `
+			height: ${element.height};
+			width: ${element.width};
+			background: ${element.bg};
+			font-size: ${element.fontSize};
+		`;
+	};
+	if(this.selector[0] === '.') {
+		const div = document.createElement('div');
+		div.className = this.selector.slice(1);
+		addSelector(div);
+		return div;
+	} else if(this.selector[0] === '#') {
+		const p = document.createElement('p');
+		p.id = this.selector.slice(1);
+		addSelector(p);
+		return p;
+	}
+};
+wrap.append(element.createTag());
