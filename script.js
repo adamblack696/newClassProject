7a4770f35a93b5cb4140c373f46e1aa76ef6a97a
Adam <sinegamy11@gmail.com>
2020-07-08 15:50:58 +0700

    fix

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const wrap = document.querySelector('.wrap');
-const DomElement = function(selector, height, width, bg, fontSize, content) {
+const DomElement = function(selector, height, width, bg, fontSize, content, wrap) {
 	 this.selector = selector;
 	 this.height = height;
 	 this.width = width;
 	 this.bg = bg;
 	 this.fontSize = fontSize;
 	 this.content = content;
+	 this.wrap = wrap;
 }
 DomElement.prototype.createTag = function() {
 	const addSelector = function(elem) {
@@ -20,20 +21,18 @@ DomElement.prototype.createTag = function() {
 			font-size: ${_this.fontSize};
 		`;
 		elem.textContent = _this.content;
+		_this.wrap.append(elem);
 	};
 	if(this.selector[0] === '.') {
 		const div = document.createElement('div');
 		div.className = this.selector.slice(1);
 		addSelector.call(this, div);
-		return div;
 	} else if(this.selector[0] === '#') {
 		const p = document.createElement('p');
 		p.id = this.selector.slice(1);
 		addSelector.call(this, p);
-		return p;
 	}
 };
 const content = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
-const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content);
-const tag = element.createTag();
-wrap.append(tag);
+const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content, wrap);
+element.createTag();
