90af31254c202ec9832733d3eb3e64a84e6e7f92
Adam <sinegamy11@gmail.com>
2020-07-08 16:02:54 +0700

    fix

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const DomElement = function(selector, height, width, bg, fontSize, content, wrap
 	 this.bg = bg;
 	 this.fontSize = fontSize;
 	 this.content = content;
-	 this.wrap = wrap;
+	 this.wrap = document.querySelector(wrap);
 }
 DomElement.prototype.createTag = function() {
 	const addSelector = function(elem) {
@@ -34,5 +34,5 @@ DomElement.prototype.createTag = function() {
 	}
 };
 const content = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
-const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content, wrap);
+const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content, '.wrap');
 element.createTag();
