d0359b2009c0820c06186878dc0940af18a94399
Adam <sinegamy11@gmail.com>
2020-07-08 15:09:37 +0700

    fix

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 'use strict';
 
 const wrap = document.querySelector('.wrap');
-const DomElement = function(selector, height, width, bg, fontSize) {
+const DomElement = function(selector, height, width, bg, fontSize, content) {
 	 this.selector = selector;
 	 this.height = height;
 	 this.width = width;
 	 this.bg = bg;
 	 this.fontSize = fontSize;
+	 this.content = content;
 }
 DomElement.prototype.createTag = function() {
 	const addSelector = function(elem) {
+		const _this = this;
+		console.log(_this);
 		elem.style.cssText = `
-			height: ${element.height};
-			width: ${element.width};
-			background: ${element.bg};
-			font-size: ${element.fontSize};
+			height: ${_this.height};
+			width: ${_this.width};
+			background: ${_this.bg};
+			font-size: ${_this.fontSize};
 		`;
+		elem.textContent = _this.content;
 	};
 	if(this.selector[0] === '.') {
 		const div = document.createElement('div');
 		div.className = this.selector.slice(1);
-		addSelector(div);
+		addSelector.call(this, div);
 		return div;
 	} else if(this.selector[0] === '#') {
 		const p = document.createElement('p');
 		p.id = this.selector.slice(1);
-		addSelector(p);
+		addSelector.call(this, p);
 		return p;
 	}
 };
-const element = new DomElement('.class-name', '100px', '370px', 'red', '16px');
+const content = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
+const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content);
 const tag = element.createTag();
-tag.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
 wrap.append(tag);
