e613a1359d8e5d18097367de8f890f0956c83132
Adam <sinegamy11@gmail.com>
2020-07-28 21:14:25 +0700

    lessonAdd

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 'use strict';
 
-const wrap = document.querySelector('.wrap');
-const DomElement = function(selector, height, width, bg, fontSize, content, wrap) {
+const DomElement = function(selector = '', height = '', width = '', bg = '', fontSize = '', content = '', wrap = '', position = '') {
 	 this.selector = selector;
 	 this.height = height;
 	 this.width = width;
 	 this.bg = bg;
 	 this.fontSize = fontSize;
 	 this.content = content;
-	 this.wrap = document.querySelector(wrap);
+	 this.position = position;
+	 this.wrap = wrap;
+	 this.left = 0;
+	 this.top = 0;
 }
 DomElement.prototype.createTag = function() {
 	const addSelector = function(elem) {
-		const _this = this;
-		console.log(_this);
+		const wrap = document.createElement('div');
+		wrap.classList.add(this.wrap);
+		document.body.append(wrap);
 		elem.style.cssText = `
-			height: ${_this.height};
-			width: ${_this.width};
-			background: ${_this.bg};
-			font-size: ${_this.fontSize};
+			height: ${this.height};
+			width: ${this.width};
+			background: ${this.bg};
+			font-size: ${this.fontSize};
+			position: ${this.position};
+			left: ${this.left};
+			right: ${this.right};
+			top: ${this.top};
+			bottom: ${this.bottom};
 		`;
-		elem.textContent = _this.content;
-		_this.wrap.append(elem);
+		elem.textContent = this.content;
+		wrap.append(elem);
+		this.elem = elem;
 	};
 	if(this.selector[0] === '.') {
 		const div = document.createElement('div');
@@ -32,7 +41,30 @@ DomElement.prototype.createTag = function() {
 		p.id = this.selector.slice(1);
 		addSelector.call(this, p);
 	}
+	this.handler();
 };
-const content = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi in, maxime, exercitationem dolorum soluta incidunt quasi repudiandae ea eaque possimus veritatis voluptatum nostrum accusantium. Ea magnam corrupti sunt sapiente laudantium';
-const element = new DomElement('.class-name', '100px', '370px', 'red', '16px', content, '.wrap');
-element.createTag();
+DomElement.prototype.handler = function() {
+	document.addEventListener('keydown', event => {
+		console.log(event.keyCode);
+		if (event.keyCode === 37) {
+			this.left -= 10;
+			this.elem.style.left = this.left + 'px';
+		}
+		if (event.keyCode === 39) {
+			this.left += 10;
+			this.elem.style.left = this.left + 'px';
+		}
+		if (event.keyCode === 38) {
+			this.top -= 10;
+			this.elem.style.top = this.top + 'px';
+		}
+		if (event.keyCode === 40) {
+			this.top += 10;
+			this.elem.style.top = this.top + 'px';
+		}
+	});
+};
+const element = new DomElement('.class-name', '100px', '100px', 'green', '16px', '', 'wrap', 'absolute');
+document.addEventListener('DOMContentLoaded', () => {
+	element.createTag.call(element);
+})
